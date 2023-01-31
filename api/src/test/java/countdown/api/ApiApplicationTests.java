325143947667df00e742e961bcbe316562416b9b
rthame <ronaldothame1@gmail.com>
2023-01-31 13:48:28 -0300

    first commit

---
+++ api/src/test/java/countdown/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package countdown.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
