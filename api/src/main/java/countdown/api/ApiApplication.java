12ab0a3e3c87d39fc2fb105211750c64fa820611
rthame <ronaldothame1@gmail.com>
2023-02-01 14:50:18 -0300

    adicionando elementos CRUD (GET/DELETE)

--- api/src/main/java/countdown/api/ApiApplication.java
+++ api/src/main/java/countdown/api/ApiApplication.java
@@ -12,7 +12,6 @@ public class ApiApplication {
 
 		SpringApplication.run(ApiApplication.class, args);
 
-
 	}
 
 }
--- api/src/main/java/countdown/api/controller/Countdown.java
+++ api/src/main/java/countdown/api/controller/Countdown.java
@@ -1,17 +1,40 @@
 package countdown.api.controller;
 
+
 import countdown.api.Tempo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.io.File;
 
 @RestController
 @RequestMapping("countdown")
 public class Countdown {
     @PostMapping
-    public Long timeInput(@RequestBody Tempo starterTime){
-        Long finalTime = starterTime.eventTime() - System.currentTimeMillis();
-        return finalTime;
+    public void timeInput(@RequestBody Tempo starterTime) throws IOException {
+            Long currentEvent = starterTime.eventTime();
+            BufferedWriter writer = new BufferedWriter(new FileWriter("eventFile.txt", true));
+            writer.append(currentEvent + "\n");
+            writer.close();
         }
-}
+    @GetMapping
+    public ArrayList<Long> eventList () throws IOException {
+        Path path = Paths.get("/Users/ronaldo.thame/countdown_timer/eventFile.txt");
+        ArrayList <Long> eventTimeList = new ArrayList<>();
+        Files.readAllLines(path).forEach(item -> eventTimeList.add(Long.valueOf(item) - System.currentTimeMillis()));
+        return eventTimeList;
+    }
+    @DeleteMapping
+    public void deleteEventList () {
+        File eventToDelete = new File("/Users/ronaldo.thame/countdown_timer/eventFile.txt");
+        boolean done = eventToDelete.delete();
+    }
+
+    }
+
